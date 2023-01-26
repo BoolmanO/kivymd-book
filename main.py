ca8de1affb44fa0035f884cbbb93877dde25e56d
BoolmanO <ponos585858@gmail.com>
2023-01-26 18:40:14 +0600

    Hello world app

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
-# !hot_reload because i dont found examples of this on win
-# init commit
+from kivymd.app import MDApp
+from kivy.lang.builder import Builder
+#from kivymd.theming import ThemeManager
+
+class HelloWorldApp(MDApp):
+    
+    def setup_theme(self):
+         self.theme_cls.theme_style = "Dark"
+         
+    def build(self):
+        self.setup_theme()
+        return Builder.load_file("frontend/main.kv")
+    
+if __name__ == '__main__':
+    HelloWorldApp().run()
