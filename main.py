cef83a7c3b736d325c9195a5c9c3d23a80c26479
BoolmanO <ponos585858@gmail.com>
2023-02-13 17:13:36 +0600

    /

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ from glob import glob
 from screens import MainScreen
 from frontend.widgets.widgets_py import *
 
-from kivy.core.window import Window # ONLY FOR DEV
-Window.size = (300, 600)
+from kivy.core.window import Window
+Window.size = (300, 600) # ONLY FOR DEV
 
-def builder_load():
+def builder_load() -> None:
     "loads all files from /frontend, use join for run this code on win and unix"
     for file in glob(join("frontend","widgets","*.kv")):
         Builder.load_file(file)
@@ -18,20 +18,24 @@ def builder_load():
 
 class BookApp(MDApp):
     
+    window = Window
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs) # MDApp create self.theme_cls in __init__
         self.setup_theme() # If swapped it won't work
         self.setup_sm()
+        self.theme_cls.material_style = "M3"
         
-    def setup_theme(self):
-        self.theme_cls.theme_style = "Dark" # TODO: make in future more themes
+
+    def setup_theme(self) -> None:
+        self.theme_cls.theme_style = "Light" # TODO: make in future more themes
         self.theme_cls.primary_palette = "Indigo"
          
-    def setup_sm(self):
+    def setup_sm(self) -> None:
         self.sm = ScreenManager()
         self.sm.add_widget(MainScreen()) # see names in screens.screens
          
-    def build(self): # kivy requires a build function
+    def build(self) -> ScreenManager: # kivy requires a build function
         return self.sm
     
 if __name__ == '__main__':
