d7d2fa3e92fb9e97dc22b75d60e70c591b8fe115
BoolmanO <ponos585858@gmail.com>
2023-02-24 18:10:36 +0600

    small changes at GoToLessonCard

--- utils/fetch_metadata.py
+++ utils/fetch_metadata.py
@@ -1,4 +1,4 @@
-from sys import platform, argv, version_info
+from sys import platform, version_info
 from os import name, getcwd
 from kivymd._version import __version__
 from kivy.config import version
@@ -7,7 +7,6 @@ from kivy.config import version
 def get_metadata():
     return {
         "platform": platform,
-        "argv": argv,
         "python": f"{version_info[0]}.{version_info[1]}.{version_info[2]}",
         "osname": name,
         "directory": getcwd(),
