f5bb1faa9915854ba9654699b788fe5b7575dfc2
BoolmanO <ponos585858@gmail.com>
2023-03-23 18:38:55 +0700

    added some comments and URL's

--- kv_annotations/__init__.py
+++ kv_annotations/__init__.py
@@ -4,7 +4,11 @@ from . import like
 """
 Don't do this in your projects, it's a very bad decision. 
 This module is a product of my laziness regarding reading the documentation.
+
 I implemented the idea of ​​callback functions with arguments with a separate class, 
 instead use the standard functools library and the partial function (functools.partial).
+https://docs.python.org/3/library/functools.html#functools.partial
+
 Instead of pseudo annotations, I recommend you use properties from kivy.properties.
+https://kivy.org/doc/stable/api-kivy.properties.html
 """
--- py_screens/__init__.py
+++ py_screens/__init__.py
@@ -1,1 +1,8 @@
 from .screens import *
+
+"""
+This module must be located in the view, in order to avoid my mistakes, 
+I highly recommend that you read 
+https://kivymd.readthedocs.io/en/latest/api/kivymd/tools/patterns/create_project/
+
+"""
