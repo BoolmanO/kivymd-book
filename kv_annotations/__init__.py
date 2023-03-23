c8422743c2d528471a15ad99f273fdeb0bd24210
BoolmanO <ponos585858@gmail.com>
2023-03-23 18:30:43 +0700

    Added warning comment at kv_annotations.__init__.py

--- kv_annotations/__init__.py
+++ kv_annotations/__init__.py
@@ -1,2 +1,10 @@
 from .functions import func_wrap
 from . import like
+
+"""
+Don't do this in your projects, it's a very bad decision. 
+This module is a product of my laziness regarding reading the documentation.
+I implemented the idea of ​​callback functions with arguments with a separate class, 
+instead use the standard functools library and the partial function (functools.partial).
+Instead of pseudo annotations, I recommend you use properties from kivy.properties.
+"""
